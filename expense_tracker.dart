c32b7dc927c341d155c1ea729e138f3e281f103c
Hemru Chamuditha <chamudhitha123@gmail.com>
2024-04-28 12:25:52 +0530

    Adding sample widgets fot the screen

--- expense_tracker.dart
+++ expense_tracker.dart
@@ -4,7 +4,22 @@ class ExpenseTracker extends StatefulWidget {
   const ExpenseTracker({super.key});
 
   @override
-  State<StatefulWidget> createState() {
-    return 
+  State<ExpenseTracker> createState() {
+    return _ExpenseTrackerState();
+  }
+}
+
+class _ExpenseTrackerState extends State<ExpenseTracker> {
+  @override
+  Widget build(context) {
+    return const Scaffold(
+      body: Column(
+        children: [
+          Text("app bard"),
+          Text("Summry detials"),
+          Text("expenses"),
+        ],
+      ),
+    );
   }
 }
