1242a459641912032a6875b062ceeff27e1a5279
Hemru Chamuditha <chamudhitha123@gmail.com>
2024-05-14 14:53:53 +0530

    Added Color Scehme to the app

--- main.dart
+++ main.dart
@@ -1,12 +1,21 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 129, 251, 208),
+);
+
 void main() {
   runApp(
     MaterialApp(
       theme: ThemeData().copyWith(
         useMaterial3: true,
-        scaffoldBackgroundColor: const Color.fromARGB(255, 212, 250, 213),
+        scaffoldBackgroundColor: kColorScheme.outlineVariant,
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
       ),
       home: const Expenses(),
     ),
