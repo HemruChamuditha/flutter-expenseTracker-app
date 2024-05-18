43a08c1e14e8b2c39583d8895e54d99ba231f676
Hemru Chamuditha <chamudhitha123@gmail.com>
2024-05-18 08:13:22 +0530

    ..

--- main.dart
+++ main.dart
@@ -2,6 +2,7 @@ import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
 var kColorScheme = ColorScheme.fromSeed(
+  //fromARGB is a alternative CONSTRUCTOR FUNCTION
   seedColor: const Color.fromARGB(255, 0, 115, 255),
 );
 
@@ -15,11 +16,13 @@ void main() {
           backgroundColor: kColorScheme.onPrimaryContainer,
           foregroundColor: kColorScheme.secondaryContainer,
         ),
+        //copyWith is a OBJECT FUNCTION
         cardTheme: const CardTheme().copyWith(
           color: const Color.fromARGB(115, 8, 44, 105),
           margin: const EdgeInsets.fromLTRB(13, 4, 13, 8),
         ),
         elevatedButtonTheme: ElevatedButtonThemeData(
+          //styleFrom static method
           style: ElevatedButton.styleFrom(
             backgroundColor: kColorScheme.secondaryContainer,
             foregroundColor: kColorScheme.onPrimaryContainer,
