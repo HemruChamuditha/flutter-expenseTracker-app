988ea4b25aba7a6afbb148ac70474343a36d9db6
Hemru Chamuditha <chamudhitha123@gmail.com>
2024-05-22 23:53:38 +0530

    locking the device orientation

--- main.dart
+++ main.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 var kColorScheme = ColorScheme.fromSeed(
   //fromARGB is a alternative CONSTRUCTOR FUNCTION
@@ -11,64 +12,69 @@ var kDarkColorScheme = ColorScheme.fromSeed(
 );
 
 void main() {
-  runApp(
-    MaterialApp(
-      darkTheme: ThemeData.dark().copyWith(
-        useMaterial3: true,
-        colorScheme: kDarkColorScheme,
-        cardTheme: const CardTheme().copyWith(
-          color: const Color.fromARGB(115, 8, 44, 105),
-          margin: const EdgeInsets.fromLTRB(16, 4, 16, 8),
-        ),
-        
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          //styleFrom static method
-          style: ElevatedButton.styleFrom(
-            backgroundColor: kDarkColorScheme.onSecondaryContainer,
-            foregroundColor: kDarkColorScheme.primaryContainer,
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+    [
+      DeviceOrientation.portraitUp,
+    ],
+  ).then(
+    (fn) => runApp(
+      MaterialApp(
+        darkTheme: ThemeData.dark().copyWith(
+          useMaterial3: true,
+          colorScheme: kDarkColorScheme,
+          cardTheme: const CardTheme().copyWith(
+            color: const Color.fromARGB(115, 8, 44, 105),
+            margin: const EdgeInsets.fromLTRB(16, 4, 16, 8),
           ),
-        ),
-        textButtonTheme: TextButtonThemeData(
-          //styleFrom static method
-          style: ElevatedButton.styleFrom(
-            
-            foregroundColor: kDarkColorScheme.onSecondaryContainer,
-          ),
-        ),
-      ),
-
-      //light theme setting by default
-      theme: ThemeData().copyWith(
-        useMaterial3: true,
-        colorScheme: kColorScheme,
-        appBarTheme: const AppBarTheme().copyWith(
-          backgroundColor: kColorScheme.onPrimaryContainer,
-          foregroundColor: kColorScheme.secondaryContainer,
-          titleTextStyle: const TextStyle(
-            fontSize: 16,
-          ),
-        ),
-        //copyWith is a OBJECT FUNCTION
-        cardTheme: const CardTheme().copyWith(
-          color: const Color.fromARGB(115, 8, 44, 105),
-          margin: const EdgeInsets.fromLTRB(23, 4, 23, 8),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          //styleFrom static method
-          style: ElevatedButton.styleFrom(
-            backgroundColor: kColorScheme.secondaryContainer,
-            foregroundColor: kColorScheme.onPrimaryContainer,
-          ),
-        ),
-        textTheme: ThemeData().textTheme.copyWith(
-              titleLarge: TextStyle(
-                fontWeight: FontWeight.normal,
-                fontSize: 23,
-                color: kColorScheme.onPrimary,
-              ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            //styleFrom static method
+            style: ElevatedButton.styleFrom(
+              backgroundColor: kDarkColorScheme.onSecondaryContainer,
+              foregroundColor: kDarkColorScheme.primaryContainer,
             ),
+          ),
+          textButtonTheme: TextButtonThemeData(
+            //styleFrom static method
+            style: ElevatedButton.styleFrom(
+              foregroundColor: kDarkColorScheme.onSecondaryContainer,
+            ),
+          ),
+        ),
+
+        //light theme setting by default
+        theme: ThemeData().copyWith(
+          useMaterial3: true,
+          colorScheme: kColorScheme,
+          appBarTheme: const AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.onPrimaryContainer,
+            foregroundColor: kColorScheme.secondaryContainer,
+            titleTextStyle: const TextStyle(
+              fontSize: 16,
+            ),
+          ),
+          //copyWith is a OBJECT FUNCTION
+          cardTheme: const CardTheme().copyWith(
+            color: const Color.fromARGB(115, 8, 44, 105),
+            margin: const EdgeInsets.fromLTRB(23, 4, 23, 8),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            //styleFrom static method
+            style: ElevatedButton.styleFrom(
+              backgroundColor: kColorScheme.secondaryContainer,
+              foregroundColor: kColorScheme.onPrimaryContainer,
+            ),
+          ),
+          textTheme: ThemeData().textTheme.copyWith(
+                titleLarge: TextStyle(
+                  fontWeight: FontWeight.normal,
+                  fontSize: 23,
+                  color: kColorScheme.onPrimary,
+                ),
+              ),
+        ),
+        home: const Expenses(),
       ),
-      home: const Expenses(),
     ),
   );
 }
